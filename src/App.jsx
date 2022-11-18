1b8b0b13db156dbcd45998dc2294ec9cfbaf834a
Andrii <A.I.Khmelovsky@gmail.com>
2022-11-18 19:03:10 +0200

    checked property functional added

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ import PropertyCard from './PropertyCard';
 function App() {
   const [properties, setProperties] = useState();
   const [filtered, setFiltered] = useState('');
+  const [savedItems, setSavedItems] = useState([]);
 
-  // use this state to keep track of the user's saved/bookmarked properties
-  const [savedProperties, setSavedProperties] = useState([]);
+  // use this state to keep track of the user's saved/bookmarked properties;
 
   useEffect(() => {
     const fetchPropertyData = async () => {
@@ -34,12 +34,27 @@ function App() {
       : properties;
   }
 
+
+  const savedProp = (id) => {
+   return savedItems.includes(id)
+      ? setSavedItems(savedItems.filter((item) => item !== id))
+      : setSavedItems([...savedItems, id]);
+  };
+
   return (
     <div className="container mx-auto my-5">
       <Header setFiltered={setFiltered} />
 
       <div className="grid grid-cols-1 gap-4 mt-5 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        {!!properties && filteredData().map((property) => <PropertyCard key={property.property_id} property={property} />)}
+        {!!properties &&
+          filteredData().map((property) => (
+            <PropertyCard
+              key={property.property_id}
+              property={property}
+              savedProp={savedProp}
+              checked={savedItems.includes(property.property_id)}
+            />
+          ))}
       </div>
     </div>
   );
--- src/PropertyCard.jsx
+++ src/PropertyCard.jsx
@@ -1,24 +1,34 @@
 import { FaBookmark } from 'react-icons/fa';
 import { Placeholder } from './Placeholder';
 
-function PropertyCard({ property }) {
+function PropertyCard({ property, checked, savedProp  }) {
 
-  const { photos, display_address, } = property;
+  const { photos, display_address, property_id } = property;
 
   return (
     <div className="border-2 bg-gray-50">
       <div className="relative h-[calc(100%_-_40px)]">
+        {photos.length ? (
+          <img
+            src={`https://mr0.homeflow.co.uk/${photos[0]}`}
+            alt={display_address}
+            className="rounded-t-lg object-cover h-full w-full"
+          />
+        ) : (
+          <Placeholder />
+        )}
 
-      {photos.length 
-          ? <img src={`https://mr0.homeflow.co.uk/${photos[0]}`} alt={display_address} className='rounded-t-lg object-cover h-full w-full' /> 
-          : <Placeholder />
-        }
-
-        <button className="absolute top-0 right-2" title="Click to bookmark this property">
-          <FaBookmark className="text-yellow-400" size="40" />
+        <button
+          className="absolute top-0 right-2"
+          title="Click to bookmark this property"
+          onClick={() => savedProp(property_id)}
+        >
+          <FaBookmark className={checked ? "text-red-400" : "text-yellow-400"}  size="40" />
         </button>
 
-        <p className="absolute bottom-0 right-0 px-2 py-1 border-t border-l bg-gray-50">{property.price}</p>
+        <p className="absolute bottom-0 right-0 px-2 py-1 border-t border-l bg-gray-50">
+          {property.price}
+        </p>
       </div>
 
       <div className="px-3 py-2">
