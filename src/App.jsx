176b38ad15ba78f8548b9e01848d0e5663719063
Andrii <A.I.Khmelovsky@gmail.com>
2022-11-18 18:34:02 +0200

    Filtering items

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import PropertyCard from './PropertyCard';
 
 function App() {
   const [properties, setProperties] = useState();
+  const [filtered, setFiltered] = useState('');
 
   // use this state to keep track of the user's saved/bookmarked properties
   const [savedProperties, setSavedProperties] = useState([]);
@@ -19,12 +20,26 @@ function App() {
     fetchPropertyData();
   }, []);
 
+
+  const filteredData = () => {
+    const Items = (item) => 
+      item.short_description 
+        ? item.short_description.toLowerCase().includes(
+            filtered.toLowerCase()
+          ) 
+        : null;
+
+    return filtered
+      ? properties.filter(Items) 
+      : properties;
+  }
+
   return (
     <div className="container mx-auto my-5">
-      <Header />
+      <Header setFiltered={setFiltered} />
 
       <div className="grid grid-cols-1 gap-4 mt-5 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        {!!properties && properties.map((property) => <PropertyCard key={property.property_id} property={property} />)}
+        {!!properties && filteredData().map((property) => <PropertyCard key={property.property_id} property={property} />)}
       </div>
     </div>
   );
